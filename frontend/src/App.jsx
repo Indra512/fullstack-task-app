023118cef25bd892469f854f2ce9a231733f751f
Indra Prajapati <workwithindra5@gmail.com>
2025-10-09 10:21:40 +0530

    update.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = process.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";;
 
 function App() {
   const [tasks, setTasks] = useState([]);
