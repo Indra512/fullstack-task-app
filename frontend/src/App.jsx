4eb76b9749172800076cd46521ccebb6808fa7d0
Indra Prajapati <workwithindra5@gmail.com>
2025-10-09 10:26:44 +0530

    Fixed import issue.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";;
+import { API_URL } from "./config";
 
 function App() {
   const [tasks, setTasks] = useState([]);
